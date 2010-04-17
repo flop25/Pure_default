78af45a22c5f375708463f873082cb8a8f2f8547
flop25 <flop25@piwigo.org>
2010-04-17 16:57:40 +0000

    Pure_defaut official
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_default@5907 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,11 @@
+<?php
+$themeconf = array(
+  'name'         => 'Pure_default',
+  'parent'        => 'default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+  'local_head'    => 'local_head.tpl',
+  'activable' => false,
+);
+
+?>
